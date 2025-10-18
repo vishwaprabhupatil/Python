624d0f5f327c67264e410ebc87a020f0d5563388
vishwaprabhupatil <vishwaprabhupatil@gmail.com>
2025-10-18 20:57:26 +0530

    Project.py

--- Tip calculator/Project.py
+++ Tip calculator/Project.py
@@ -3,5 +3,5 @@ a=float(input("What was the total bill?\n$"))
 b=int(input("What percentage tip would you like to give? 10, 12 or 15\n"))
 c=int(input("How many people are splitting the bill?\n"))
 total=a+(a*(b/100))
-split=total/c
+split=round(total/c, 2)
 print(f"Each person should pay: ${split}")

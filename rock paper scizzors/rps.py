7923c5840387c286047fd447436de0e885769508
vishwaprabhupatil <vishwaprabhupatil@gmail.com>
2025-11-01 12:49:13 +0530

    Edited rps

--- rock paper scizzors/rps.py
+++ rock paper scizzors/rps.py
@@ -25,22 +25,11 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
+game_image=[rock,paper,scissors]
 a=int(input("What do you choose? Type 0 for Rock, 1 for Papers and 2 for scissors\n"))
-if a==0:
-    print("Your choice:\n\n",rock,"\n\n")
-elif a==1:
-    print("Your choice:\n\n",paper,"\n\n") 
-elif a==2:
-    print("Yours choice:\n\n",scissors,"\n\n") 
-print("Computer's choice:\n\n")
-b=random.randint(0,2)
-if b==0:
-    print(rock)
-elif b==1:
-    print(paper) 
-elif b==2:
-    print(scissors) 
+print(f"Your choice:\n\n{game_image[a]}\n\n")
+b=random.randint(0,2) 
+print(f"Computer's choice:\n\n{game_image[b]}\n")
 if a==0 and b==1:
     print("\n\nYou lose!")
 elif a==0 and b==2:

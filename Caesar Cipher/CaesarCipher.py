c39348c4c981f4b34b18e5d9e745e3285ba460d4
vishwaprabhupatil <vishwaprabhupatil@gmail.com>
2026-02-17 14:34:38 +0530

    17 Feb 26

--- Caesar Cipher/CaesarCipher.py
+++ Caesar Cipher/CaesarCipher.py
@@ -1,3 +1,23 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -8,15 +28,33 @@ def caesar(plain_txt, shift_amt, dir):
     elif dir=="decode":
         shift_amt=shift_amt*-1
     text=""
-    for letter in plain_txt:
-        pos=alphabet.index(letter)
-        newpos=pos+shift_amt
-        new_letter=alphabet[newpos]
-        text=text+new_letter
+    for char in plain_txt:
+        if char in alphabet:
+            pos=alphabet.index(char)
+            newpos=pos+shift_amt
+            new_letter=alphabet[newpos]
+            text=text+new_letter
+        else:
+            text=text+char
     print(f"The {dir}d text is {text}")    
        
         
-direction=input("Type 'encode' to encrypt, Type 'decode' to decrypt:\n")
-message=input("Type your message:\n").lower()
-shift=int(input("Type the number of shifts: "))
-caesar(plain_txt=message, shift_amt=shift,dir=direction)
+
+
+
+
+end=False
+while end==False:
+    direction=input("Type 'encode' to encrypt, Type 'decode' to decrypt:\n")
+    message=input("Type your message:\n").lower()
+    shift=int(input("Type the number of shifts: "))
+    shift=shift%26
+    caesar(plain_txt=message, shift_amt=shift,dir=direction)    
+    choice=input("Do you want to continue? (Y/N)\n").lower()
+    if choice=="y":
+        end=True
+    elif choice=="n":
+        end=False
+        print("\n")
+    
+        

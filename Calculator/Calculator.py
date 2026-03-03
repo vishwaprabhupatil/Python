e02f2c3a94101e10a4c283b89450c8477a6efa10
vishwaprabhupatil <vishwaprabhupatil@gmail.com>
2026-03-03 23:13:51 +0530

    3 mar 26

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,23 +1,72 @@
-print("Calculator\n")
-num1=float(input("Enter the first number\n"))
-num2=float(input("Enter the second number\n"))
-print("Choose your operation from 1,2,3 or 4 where,\n1-Addition\n2-Subtraction\n3-Multiplication\n4-Division\n")
-choice=int(input("Choose here "))
-if choice==1:
-    result=num1+num2
-    print("result=",result)
-elif choice==2:
-    result=num1-num2
-    print("result=",result)
-elif choice==3:
-    result=num1*num2
-    print("result=",result)   
-elif choice==4:
-    if num2==0:
-        print("Error!Division by zero is not defined")
-    else:
-        result=num1/num2
-        print("result=",result)
-else:
-    print("Please choose an option between 1-4")
+import math
+art=logo = """
+ _____________________
+|  _________________  |
+| |              0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1+n2 
+def sub(n1, n2):
+    return n1-n2
+
+def mult(n1,n2):
+    return n1*n2
+
+def div(n1,n2):
+    return n1/n2
+
+def expo(n1,n2):
+    return n1**n2
+
+def sqr(n1,n2):
+    return math.sqrt(n1)
+
+print(art, "\n\n\n")
+n1=float(input("Enter the first number: "))
+print("\n***Choose the operation***")
+print("Press 1 to Add\nPress 2 to Subtract\nPress 3 to Multipy\nPress 4 to Divide\nPress 5 to Raise to power(for root use '1/second number' while choosing the Second number)\nPress 6 for the square root of the first number")
+choice=input("\n")
+n2=float(input("\nEnter the second number: "))
+
+operations={"1":add, "2":sub, "3":mult, "4":div, "5":expo, "6":sqr}
+symbol={"1":f"{n1} + {n2}", "2":f"{n1} - {n2}", "3":f"{n1} * {n2}", "4": f"{n1}/{n2}", "5":f"({n1})^({n2})", "6": f"√{n1}"}
+
+function=operations[choice]
+print(f"\n {symbol[choice]} = {function(n1,n2)}")
+
+
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Leap year/leap.py
+++ Leap year/leap.py
@@ -1,13 +1,29 @@
 print("Leap year checker\n")
 a=int(input("Enter the year\n"))
-if a%4==0:
-    if a%100==0:
-        if a%400==0:
-            print("The given year is a leap year")
+b=int(input("Enter a month: "))
+
+def leap(a):
+    if a%4==0:
+        if a%100==0:
+            if a%400==0:
+                return True
+            else:
+                return False
         else:
-            print("The given year is not a leap year")
+            return True
     else:
-        print("The given year is a leap year")
-else:
-    print("The given year is not a leap year")
+        return False
                 
+def days_in_month(month):
+    month_days=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if leap(a) and month==2:
+        return 29
+    return month_days[month-1]
+days=days_in_month(b)
+year=""
+if leap(a):
+    year="leap year"
+else:
+    year="not a leap year"
+print(f"the given year is {year} and the given month has {days} days")
+
